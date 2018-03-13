2250a07a35045fcf8c5c6c16a1d5c44866e49c1e
snopeks <creativesnopeks@gmail.com>
2018-03-13 10:45:51 -0700

    completed CRUDing

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,128 @@
+var $foodList;
+var allFoods = [];
+
 $(function(){
   console.log('app.js is connected!')
-})
+
+  $foodList = $('#foodTarget');
+  $.ajax({
+    method: 'GET',
+    url: '/api/food',
+    success: handleSuccess,
+    error: handleError
+  })
+  $("#create-new-food").on('submit', function(e){
+    e.preventDefault();
+    $.ajax({
+      method: "POST",
+      url: '/api/food',
+      data: $(this).serializeArray(),
+      success: postSuccess,
+      error: postError
+    })
+  })
+
+  $foodList.on('click', '.edit-food-button', function(){
+    console.log('clicked edit button');
+    $(this).parent().find('.edit-input').show();
+  })
+  $foodList.on('click', '.edit-food-submit-button', function(){
+    console.log("clicked save button");
+    $(this).parent().hide();
+    var newName = $(this).parent().find("input").val();
+    console.log($(this))
+    console.log(newName)
+    console.log($(this).parent().parent())
+    var than = this;
+    $.ajax({
+      method: "PUT",
+      url: `/api/food/${ $(this).attr('data-id') }`,
+      data: { name: newName },
+      success: function(food){
+        console.log("this is food name", food.name)
+        $(than).parent().parent().find('.food-name').html(food.name)
+      },
+      error: handleUpdateError
+    })
+  })
+  $foodList.on('click', '.deleteBtn', function(){
+    console.log('clicked delete button for food', '/api/food/'+$(this).attr('data-id'));
+    $.ajax({
+      method: 'DELETE',
+      url: 'api/food/' + $(this).attr('data-id'),
+      success: deleteFoodSuccess,
+      error: deleteFoodError
+    })
+  })
+
+}) //end of onReady
+function deleteFoodSuccess(json){
+  console.log('this is your json', json)
+  var food = json;
+  var foodId = food._id;
+  console.log('delete food', foodId)
+  for(var index = 0; index < allFoods.length; index++){
+    if(allFoods[index]._id === foodId){
+      allFoods.splice(index, 1);
+      break
+    }
+  }
+  render();
+}
+
+function deleteFoodError(err){
+  console.log("there was an error deleting your food!", err)
+}
+
+function handleUpdateSuccess(food){
+  console.log("this is food", food)
+  console.log('this is food.name', food.name)
+  $(this).parent().parent().find('.food-name').html(food.name)
+}
+function handleUpdateError(){
+  console.log("there was an error updating your food")
+}
+
+function getFoodHtml(food){
+  return `<hr>
+          <p>
+            <b class="food-name">${food.name}</b>
+            <span class="edit-input" style="display: none">
+              <input type="text" value="${food.name}" />
+              <button class="edit-food-submit-button" data-id="${food._id}">Save</button>
+            </span>
+            <button class="edit-food-button">Edit</button>
+            <br>
+            <button type="button" name="button" class="deleteBtn btn btn-danger pull-right" data-id="${food._id}">Delete</button>
+          </p>
+          `;
+}
+
+function getAllFoodHtml(food){
+  return food.map(getFoodHtml).join("");
+}
+
+function render() {
+  $foodList.empty();
+  var foodHtml = getAllFoodHtml(allFoods);
+  $foodList.append(foodHtml);
+}
+
+function handleSuccess(json){
+  allFoods = json;
+  render();
+}
+function handleError(e){
+  console.log('uh oh');
+  $("#foodTarget").text('Failed to load food, is the server working?');
+}
+
+function postSuccess(json){
+  console.log(json)
+  $('#create-new-food input').val('')
+  allFoods.push(json)
+  render();
+}
+function postError(err){
+  console.log("there was an error!", err)
+}
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express'),
 
 var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}));
-// var db = require('./models')
+var db = require('./models')
 
 app.use(function(req, res, next){
   res.header('Access-Control-Allow-Origin', '*');
@@ -17,6 +17,7 @@ app.use(express.static(__dirname + '/views'));
 app.get('/', function homepage(req, res){
   console.log('in the homepage fn!')
   res.status(200).render('index')
+
 })
 
 app.get('/api', function showApi(req, res){
@@ -25,7 +26,9 @@ app.get('/api', function showApi(req, res){
     endpoints: [
       {method: 'GET', path: '/api', description: 'Describes all available endpoints'},
       {method: 'GET', path: '/api/profile', description: 'gives my developer profile info'},
-      {method: 'POST', path: 'api/foods', description: 'create a new fav food'}
+      {method: 'POST', path: 'api/food', description: 'create a new fav food'},
+      {method: 'PUT', path: 'api/food/:id', description: 'update a specific food'},
+      {method: 'DELETE', path: 'api/food/:id', description: 'delete a food'}
     ]
   }
   res.json({myApi})
@@ -40,6 +43,53 @@ app.get('/api/profile', function myProfile(req, res){
   res.json({profile})
 })
 
+app.get('/api/food', function favFoods(req, res){
+  db.Food.find().exec(function getAllFood(err, allFood){
+    if(err){
+      return console.log('there was an error!', err)
+    } else {
+      res.json(allFood)
+    }
+  })
+})
+app.post('/api/food', function addFood(req, res){
+  console.log(req.body)
+  db.Food.create({
+    name: req.body.name,
+    ingredients: req.body.ingredients,
+    countryOfOrigin: req.body.countryOfOrigin,
+    recipeUrl: req.body.recipeUrl,
+    rating: req.body.rating
+  }, function(err, succ){
+    if(err){
+      console.log("there was an error creating your food", err)
+    } else {
+      console.log("created food", succ)
+      res.json(succ)
+    }
+  })
+  // res.redirect('/')
+})
+
+app.put('/api/food/:id', function editFood(req, res){
+  console.log("food edit!", req.params);
+  console.log('body is ', req.body);
+  var foodId = req.params.id;
+  db.Food.findOneAndUpdate({ _id: foodId }, req.body, {new: true})
+    .exec(function(err, updatedFood){
+      console.log(updatedFood)
+      res.json(updatedFood)
+    });
+})
+
+app.delete('/api/food/:id', function deleteFood(req, res){
+  console.log('this is your req.params.id', req.params.id)
+  var foodId = req.params.id
+  db.Food.findOneAndRemove({_id: foodId}, function(err, deletedFood){
+    res.json(deletedFood)
+  })
+
+})
 app.listen(process.env.PORT || 3000, function(){
   console.log("express server is up and running on http://localhost:3000/")
 });
